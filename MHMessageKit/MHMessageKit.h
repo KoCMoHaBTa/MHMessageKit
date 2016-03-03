af63b208e753b48abbe83a25b6a1c50fb121dba2
Milen Halachev <milenski88@gmail.com>
2016-03-04 00:04:00 +0200

    Initial Commit

---
+++ MHMessageKit/MHMessageKit.h
@@ -0,0 +1,19 @@
+//
+//  MHMessageKit.h
+//  MHMessageKit
+//
+//  Created by Milen Halachev on 3/4/16.
+//  Copyright © 2016 Milen Halachev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MHMessageKit.
+FOUNDATION_EXPORT double MHMessageKitVersionNumber;
+
+//! Project version string for MHMessageKit.
+FOUNDATION_EXPORT const unsigned char MHMessageKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MHMessageKit/PublicHeader.h>
+
+
